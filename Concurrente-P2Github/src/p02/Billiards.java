9a439b1632d76b8dd6a3dc7df52676a642706c0c
rma0057 <rma0057@alu.ubu.es>
2018-03-03 21:25:40 +0100

    Init balls
    inicializacion de las bolas

--- Concurrente-P2Github/src/p02/Billiards.java
+++ Concurrente-P2Github/src/p02/Billiards.java
@@ -55,6 +55,11 @@ public class Billiards extends JFrame {
 
 	private void initBalls() {
 		// TODO init balls
+		balls=new Ball[N_BALL+3];
+		for(int i=0;i<balls.length;i++){
+			balls[i]=new Ball();
+		}
+
 	}
 
 	private class StartListener implements ActionListener {
